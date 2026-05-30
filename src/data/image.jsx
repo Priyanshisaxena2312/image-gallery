e42d19e53321a8a8da23e461d0cf6e84d431a49a
Priyanshi Saxena <priyasaxena7002@gmail.com>
2026-05-30 16:28:41 +0530

    fix image

--- src/data/image.jsx
+++ src/data/image.jsx
@@ -1,77 +1,77 @@
 const images = [
   {
     id: 1,
-    src: "public/images/1.jpg",
+    src: "/images/1.jpg",
     category: "Nature",
   },
   {
     id: 2,
-    src: "public/images/2.jpg",
+    src: "/images/2.jpg",
     category: "City",
   },
   {
     id: 3,
-    src: "public/images/3.jpg",
+    src: "/images/3.jpg",
     category: "Animals",
   },
   {
       id: 4,
-    src: "public/images/4.jpg",
+    src: "/images/4.jpg",
     category: "Nature",
   },
   {
       id: 5,
-    src: "public/images/5.jpg",
+    src: "/images/5.jpg",
     category: "Nature",
   },
   {
       id: 6,
-    src: "public/images/6.jpg",
+    src: "/images/6.jpg",
     category: "Nature",
   },
   {
       id: 7,
-    src: "public/images/7.jpg",
+    src: "/images/7.jpg",
     category: "Nature",
   },
   {
     id: 8,
-    src: "public/images/8.jpg",
+    src: "/images/8.jpg",
     category: "City",
   },
   {
     id: 9,
-    src: "public/images/9.jpg",
+    src: "/images/9.jpg",
     category: "City",
   },
   {
     id: 10,
-    src: "public/images/10.jpg",
+    src: "/images/10.jpg",
     category: "City",
   },
   {
     id: 11,
-    src: "public/images/11.jpg",
+    src: "/images/11.jpg",
     category: "City",
   },
   {
     id: 12,
-    src: "public/images/12.jpg",
+    src: "/images/12.jpg",
     category: "Animals",
   },
   {
     id: 13,
-    src: "public/images/13.jpg",
+    src: "/images/13.jpg",
     category: "Animals",
   },
   {
     id: 14,
-    src: "public/images/14.jpg",
+    src: "/images/14.jpg",
     category: "Animals",
   },
   {
     id: 15,
-    src: "public/images/15.jpg",
+    src: "/images/15.jpg",
     category: "Animals",
   },
 
